f784dc5abbe0c8fb8d5ab3bcd75b1caf38ff5f84
ejhones <ejhones@icloud.com>
2017-04-28 14:45:17 -0300

    Initial Commit

---
+++ GoHotmart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoHotmart
+//
+//  Created by Edilson Jhones on 28/04/17.
+//  Copyright © 2017 Edilson Jhones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
